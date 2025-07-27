de3631ebc995b9497af35d7f3de6aff88e249cf7
NanduGoud <nandugoud33@icloud.com>
2025-07-27 15:41:32 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleBtn() {
+    const menu = document.querySelector(".menu-links");
+    const burger = document.querySelector(".humburger-icon");
+    menu.classList.toggle("open");
+    burger.classList.toggle("open");
+
+}
